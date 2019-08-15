1c121f02c721c548e5bf6e2e9b3e5e2f702bab39
BrianKohnen <briankohnen@yahoo.com>
2019-08-15 00:37:40 -0500

    bk--finished up concert-this functionality. user inputs band, information about upcoming concerts is displaying in terminal

--- liri.js
+++ liri.js
@@ -4,18 +4,22 @@ const keys = require("./keys.js");
 
 const axios = require("axios");
 
+const moment = require("moment");
+
 //const spotify = new Spotify(keys.spotify);
+
+function concertThis() {
 
 let artist = process.argv.slice(2).join(" ");
 
 axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
     .then(function(response) {
         for (var i = 0; i < response.data.length; i++) {
-            console.log(
+            console.log( 
                 "\n============================" + 
                 "\nVenue : " + response.data[i].venue.name + 
                 "\nCity : " + response.data[i].venue.city + 
-                "\nWhen : " + response.data[i].datetime + 
+                "\nWhen : " + moment(response.data[i].datetime).format("L") + 
                 "\n============================"
                 );
         }
@@ -23,3 +27,13 @@ axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=cod
     .catch(function(error) {
         console.log(error);
     });
+
+};
+
+concertThis();
+
+function spotifyThisSong() {
+
+    let song = process.argv.slice(2).join(" ");
+
+}
